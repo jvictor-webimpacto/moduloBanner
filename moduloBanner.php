eccc78a271423950a23a31f4b021fad498bf7d40
Javier <Jvictor@webimpacto.es>
2019-04-30 11:10:46 +0200

    1

--- moduloBanner.php
+++ moduloBanner.php
@@ -80,18 +80,18 @@ class ModuloBanner extends Module
 			}else {
 				return $this->html . $this->renderForm();
 			}
-		} 
+		}
 		elseif (Tools::isSubmit('updatemoduloBanner') || Tools::isSubmit('addmoduloBanner')) {
 			$this->html .= $this->renderForm();
 			return $this->html;
-		} 
+		}
 		elseif (Tools::isSubmit('deletemoduloBanner')) {
 			$banner = new Banner((int)$id_banner);
 			$banner->delete();
 			$this->_clearCache('category.tpl');
 			Tools::redirectAdmin(AdminController::$currentIndex.'&configure='.$this->name.'&token='.
 			Tools::getAdminTokenLite('AdminModules'));
-		} 
+		}
 		else {
 			$this->html .= $this->renderList();
 			return $this->html;
@@ -238,7 +238,7 @@ class ModuloBanner extends Module
 							$saved = $banner->save();
 						}
 				}
-			} 
+			}
 			else {
 				$banner = new Banner((int)$id_banner);
 				if (isset($_REQUEST['savemoduloBanner'])) {
@@ -279,7 +279,7 @@ class ModuloBanner extends Module
 			$fields_value['id_category'] = $banner->id_category;
 			$fields_value['hook'] = $banner->hook;
 			$fields_value['imagen'] = $banner->imagen;
-		} 
+		}
 		else {
 			$fields_value['id_category'] = "";
 			$fields_value['hook'] = "";
